cec4d52de214c859cf84172fdbca1786267af889
vish198910 <vish198910@gmail.com>
2020-04-23 16:12:44 +0530

    Extracting curated images

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -6,27 +6,56 @@ List<CategoryModel> getCategories(){
 
   CategoryModel category = new CategoryModel();
   category.categoryName = "Street Art";
-  category.imgUrl = "https://images.pexels.com/photos/545008/pexels-photo-545008.jpeg";
-  
+  category.imgUrl = "https://images.pexels.com/photos/545008/pexels-photo-545008.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
+  categories.add(category);
+
   category = new CategoryModel();
 
   category.categoryName = "City";
-  category.imgUrl = "https://images.pexels.com/photos/1034662/pexels-photo-1034662.jpeg";
-  
+  category.imgUrl = "https://images.pexels.com/photos/1034662/pexels-photo-1034662.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
+  categories.add(category);
+
   category = new CategoryModel();
 
   category.categoryName = "Cars";
-  category.imgUrl = "https://images.pexels.com/photos/1545743/pexels-photo-1545743.jpeg";
-  
+  category.imgUrl = "https://images.pexels.com/photos/1545743/pexels-photo-1545743.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
+  categories.add(category);
+
   category = new CategoryModel();
 
   category.categoryName = "Motivation";
-  category.imgUrl = "https://images.pexels.com/photos/2255441/pexels-photo-2255441.jpeg";
-  
+  category.imgUrl = "https://images.pexels.com/photos/2255441/pexels-photo-2255441.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
+  categories.add(category);
+
   category = new CategoryModel();
 
   category.categoryName = "Bikes";
-  category.imgUrl = "https://images.pexels.com/photos/296735/pexels-photo-296735.jpeg";
+  category.imgUrl = "https://images.pexels.com/photos/296735/pexels-photo-296735.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
+  categories.add(category);
+
+  category = new CategoryModel();
   
+  category.categoryName = "Nature";
+  category.imgUrl = "https://images.pexels.com/photos/358457/pexels-photo-358457.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
+  categories.add(category);
+
+  category = new CategoryModel();
+  
+  category.categoryName = "B & W";
+  category.imgUrl = "https://images.pexels.com/photos/57905/pexels-photo-57905.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
+  categories.add(category);
+
+  category = new CategoryModel();
+  
+  category.categoryName = "Space";
+  category.imgUrl = "https://images.pexels.com/photos/110854/pexels-photo-110854.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
+  categories.add(category);
+
+  category = new CategoryModel();
+  
+  category.categoryName = "Abstract";
+  category.imgUrl = "https://images.pexels.com/photos/1616403/pexels-photo-1616403.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
+  categories.add(category);
+
  return categories;
 }
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,11 +1,33 @@
+import 'package:flutter/foundation.dart';
 import "package:flutter/material.dart";
-
+import 'package:horizons/constant.dart';
+import 'package:horizons/data/data.dart';
+import 'package:horizons/model/categories_model.dart';
+import 'package:http/http.dart' as http;
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  List<CategoryModel> categories = new List();
+
+  getCuratedPhotos() async {
+    String curatedPhotosUrl = "https://api.pexels.com/v1/curated?per_page=25&page=1";
+    var response = await http.get(curatedPhotosUrl,headers: {
+      "Authorization":apikey
+    });
+    print(response.body.toString());
+  }
+  
+  @override
+  void initState() {
+    categories = getCategories();
+    print(categories);
+    getCuratedPhotos();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,41 +36,75 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.white,
         centerTitle: true,
         elevation: 0.0,
-        title:Text("HORIZONS",style: TextStyle(
-          color:Colors.black87
-        ),),
-
+        title: Text(
+          "HORIZONS",
+          style: TextStyle(color: Colors.black87),
+        ),
       ),
       body: Container(
-        child: Column(
-          children:<Widget>[
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 24),
-              padding: const EdgeInsets.symmetric(horizontal:16),
-        decoration: BoxDecoration(
-          color:Color(0xfff5f8fd),
-          borderRadius: BorderRadius.circular(16)
-        ),
-              child: Row(
-                children:<Widget>[
-                  Expanded(
-                           child: TextField(
-                      decoration: InputDecoration(
-                        hintText: "search"
-                      ),
-                    ),
-                  ),
-                  GestureDetector(
-                    child:Icon(
-                      Icons.search
-                    )
-                  ),
-                ]
+        child: Column(children: <Widget>[
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 24),
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            decoration: BoxDecoration(
+                color: Color(0xfff5f8fd),
+                borderRadius: BorderRadius.circular(16)),
+            child: Row(children: <Widget>[
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(hintText: "search"),
+                ),
               ),
-            ),
-          ]
-        ),
+              GestureDetector(child: Icon(Icons.search)),
+            ]),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Container(
+            height:80,
+            child: ListView.builder(
+                padding: const EdgeInsets.symmetric(horizontal:24),
+                itemBuilder: (context, index) {
+                  return CategoryTile(
+                      title: categories[index].categoryName,
+                      imgUrl: categories[index].imgUrl);
+                },
+                itemCount: categories.length,
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                ),
+          )
+        ]),
       ),
     );
+  }
+}
+
+class CategoryTile extends StatelessWidget {
+  final String imgUrl, title;
+  CategoryTile({@required this.imgUrl, @required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 4),
+        child: Stack(
+      children: <Widget>[
+        ClipRRect(
+          child: Image.network(imgUrl, height: 60, width:100, fit:BoxFit.cover),
+          borderRadius: BorderRadius.circular(10),
+          ),
+        Container(
+          alignment: Alignment.center,
+          height: 60, 
+          width:100,
+          decoration: BoxDecoration(
+            color: Colors.black26,
+            borderRadius: BorderRadius.circular(10)
+          ),
+          child: Text(title,style: TextStyle(color:Colors.white,fontWeight: FontWeight.w600,fontSize: 16),))
+      ],
+    ));
   }
 }
