ea4f28d1070113958889e47b12dd723d32aa6598
vish198910 <vish198910@gmail.com>
2020-04-27 09:02:18 +0530

    search query wallpapers work

---
+++ lib/views/search.dart
@@ -0,0 +1,90 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:horizons/constants.dart';
+import 'package:horizons/model/wallpaper_model.dart';
+import 'package:horizons/widgets/widget.dart';
+import 'package:http/http.dart' as http;
+class Search extends StatefulWidget {
+
+  final String searchQuery;
+  Search({this.searchQuery});
+
+  @override
+  _SearchState createState() => _SearchState();
+}
+
+class _SearchState extends State<Search> {
+
+
+ List<WallpaperModel> wallpapers = new List();
+ TextEditingController textEditingController = new TextEditingController();
+
+getSearchPhotos(String query) async {
+    String curatedPhotosUrl =
+        "https://api.pexels.com/v1/search?query=$query&per_page=50&page=1";
+    var response =
+        await http.get(curatedPhotosUrl, headers: {"Authorization": apikey});
+    print(response.body.toString());
+
+    Map<String, dynamic> jsonData = jsonDecode(response.body);
+
+    jsonData["photos"].forEach((element) {
+      WallpaperModel wallpaperModel = new WallpaperModel();
+      wallpaperModel = WallpaperModel.fromMap(element);
+      wallpapers.add(wallpaperModel);
+    });
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    getSearchPhotos(widget.searchQuery);
+    super.initState();
+    textEditingController.text = widget.searchQuery;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        elevation: 0.0,
+        title: Text(
+          "HORIZONS",
+          style: TextStyle(color: Colors.black87),
+        ),
+      ),
+      body: SingleChildScrollView(
+              child: Container(
+          child: Column(
+            children: <Widget>[
+              Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 24),
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      decoration: BoxDecoration(
+                          color: Color(0xfff5f8fd),
+                          borderRadius: BorderRadius.circular(16)),
+                      child: Row(children: <Widget>[
+                        Expanded(
+                          child: TextField(
+                            controller: textEditingController,
+                            decoration: InputDecoration(hintText: "search"),
+                          ),
+                        ),
+                        GestureDetector(child: Icon(Icons.search),
+                        onTap: (){
+                          wallpapers = [];
+                          getSearchPhotos(textEditingController.text);
+                        },),
+                      ]),
+                    ),
+                    SizedBox(height: 16,),
+                    WallpaperListWidget(wallpapers: wallpapers, context: context),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
